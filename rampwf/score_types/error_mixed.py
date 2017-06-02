1bb48816421f4d8e24414291ed683e929bc97cb0
Balazs Kegl <balazs.kegl@gmail.com>
2017-06-02 20:08:56 +0200

    error

--- rampwf/score_types/error_mixed.py
+++ rampwf/score_types/error_mixed.py
@@ -1,9 +1,9 @@
-from . import error
+from . import classification_error
 
 
 def score_function(ground_truths, predictions, valid_indexes=None):
     """Classification error of a mixed regression/classification prediction."""
-    return error.score_function(
+    return classification_error.score_function(
         ground_truths.multiclass,
         predictions.multiclass,
         valid_indexes)
