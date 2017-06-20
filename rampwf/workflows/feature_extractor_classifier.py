91dc2b536e4f8e0f91d8e7f3909538cb460db31b
Balazs Kegl <balazs.kegl@gmail.com>
2017-06-20 14:47:30 +0200

    Titanic ingredients (auc, feture extractor, etc)

--- rampwf/workflows/feature_extractor_classifier.py
+++ rampwf/workflows/feature_extractor_classifier.py
@@ -5,10 +5,10 @@ from .classifier import Classifier
 class FeatureExtractorClassifier(object):
     def __init__(self, workflow_element_names=[
             'feature_extractor', 'classifier']):
-        self.workflow_element_names = workflow_element_names
+        self.element_names = workflow_element_names
         self.feature_extractor_workflow = FeatureExtractor(
-            [self.workflow_element_names[0]])
-        self.classifier_workflow = Classifier([self.workflow_element_names[1]])
+            [self.element_names[0]])
+        self.classifier_workflow = Classifier([self.element_names[1]])
 
     def train_submission(self, module_path, X_df, y_array, train_is=None):
         if train_is is None:
@@ -27,5 +27,3 @@ class FeatureExtractorClassifier(object):
             fe, X_df)
         y_proba = self.classifier_workflow.test_submission(clf, X_test_array)
         return y_proba
-
-workflow = FeatureExtractorClassifier()
