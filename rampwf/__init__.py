e90e771fbc22c6fe629513e3003166e29555722b
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2017-06-21 18:00:38 +0200

    [WIP] testing backend submission (#11)
    * iter
    
    * ENH add fetcher
    
    * EHN move backend test in testing modules
    
    * FIX default directory
    
    * ENH use join instead of concatenation
    
    * FIX join
    
    * TST improve tests

--- rampwf/__init__.py
+++ rampwf/__init__.py
@@ -2,4 +2,6 @@ from . import prediction_types
 from . import score_types
 from . import workflows
 
+__all__ = ['kits', 'utils']
+
 __version__ = '0.1.dev'
---
+++ rampwf/kits/__init__.py
@@ -0,0 +1,4 @@
+from .ramp_kit import fetch_ramp_kit
+
+
+__all__ = ['fetch_ramp_kit']
