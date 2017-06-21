2603bfcbb8f71e0aa1569c30065020e5a1cd6808
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2017-06-22 00:23:23 +0200

    EHN add command line module to avoid having bin dir

--- rampwf/utils/__init__.py
+++ rampwf/utils/__init__.py
@@ -1,3 +1,5 @@
+from .command_line import ramp_test_submission
 from .testing import assert_submission
 
-__all__ = ['assert_submission']
+__all__ = ['ramp_test_submission',
+           'assert_submission']
