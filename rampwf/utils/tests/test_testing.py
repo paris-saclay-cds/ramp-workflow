4077bc7167c8f912784583fdc11c89b67af1e117
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2017-06-22 14:13:41 +0200

    TST all submission

--- rampwf/utils/tests/test_testing.py
+++ rampwf/utils/tests/test_testing.py
@@ -15,3 +15,29 @@ def test_submission_all_kits():
                               submission_name='*')
     finally:
         shutil.rmtree(tmp_dir)
+
+
+def test_submission_with_list():
+    tmp_dir = mkdtemp()
+    try:
+        kit = 'titanic'
+        kit_dir = fetch_ramp_kit(kit, ramp_kits_home=tmp_dir)
+        submission_name = ['starting_kit', 'random_forest_20_5']
+        assert_submission(ramp_kit_dir=kit_dir,
+                          ramp_data_dir=kit_dir,
+                          submission_name=submission_name)
+    finally:
+        shutil.rmtree(tmp_dir)
+
+
+def test_submission_with_str():
+    tmp_dir = mkdtemp()
+    try:
+        kit = 'titanic'
+        kit_dir = fetch_ramp_kit(kit, ramp_kits_home=tmp_dir)
+        submission_name = 'starting_kit'
+        assert_submission(ramp_kit_dir=kit_dir,
+                          ramp_data_dir=kit_dir,
+                          submission_name=submission_name)
+    finally:
+        shutil.rmtree(tmp_dir)
