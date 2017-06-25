8bf8e135b8043912b44358fc5d7646b075435787
Balazs Kegl <balazs.kegl@gmail.com>
2017-06-25 17:48:48 +0200

    1) FetureExtractorRegressor, 2) small bug in score_type/base

--- rampwf/workflows/feature_extractor_regressor.py
+++ rampwf/workflows/feature_extractor_regressor.py
@@ -5,10 +5,10 @@ from .regressor import Regressor
 class FeatureExtractorRegressor(object):
     def __init__(self, workflow_element_names=[
             'feature_extractor', 'regressor']):
-        self.workflow_element_names = workflow_element_names
+        self.element_names = workflow_element_names
         self.feature_extractor_workflow = FeatureExtractor(
-            [self.workflow_element_names[0]])
-        self.regressor_workflow = Regressor([self.workflow_element_names[1]])
+            [self.element_names[0]])
+        self.regressor_workflow = Regressor([self.element_names[1]])
 
     def train_submission(self, module_path, X_df, y_array, train_is=None):
         if train_is is None:
@@ -25,7 +25,5 @@ class FeatureExtractorRegressor(object):
         fe, reg = trained_model
         X_test_array = self.feature_extractor_workflow.test_submission(
             fe, X_df)
-        y_proba = self.regressor_workflow.test_submission(reg, X_test_array)
-        return y_proba
-
-workflow = FeatureExtractorRegressor()
+        y_pred = self.regressor_workflow.test_submission(reg, X_test_array)
+        return y_pred
