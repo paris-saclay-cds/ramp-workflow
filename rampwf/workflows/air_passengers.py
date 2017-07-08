615b85dda1bb4677742ae733b4253fcd840cc56a
Balazs Kegl <balazs.kegl@gmail.com>
2017-07-08 12:43:13 +0200

    forgot to add external data

--- rampwf/workflows/air_passengers.py
+++ rampwf/workflows/air_passengers.py
@@ -5,3 +5,4 @@ class AirPassengers(FeatureExtractorRegressor):
     def __init__(self, workflow_element_names=[
             'feature_extractor', 'regressor', 'external_data.csv']):
         super(AirPassengers, self).__init__(workflow_element_names[:2])
+        self.element_names = workflow_element_names
