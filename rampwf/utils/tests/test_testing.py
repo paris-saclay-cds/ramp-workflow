98bd17cef806ec4cd518c4fec259ae672326f0b3
Balazs Kegl <balazs.kegl@gmail.com>
2017-07-20 01:45:13 +0200

    test test

--- rampwf/utils/tests/test_testing.py
+++ rampwf/utils/tests/test_testing.py
@@ -8,14 +8,15 @@ from rampwf.kits.ramp_kit import RAMP_KITS_AVAILABLE
 def test_submission_all_kits():
     tmp_dir = mkdtemp()
     try:
+        test_notebook = True
         for kit in RAMP_KITS_AVAILABLE:
             kit_dir = fetch_ramp_kit(kit, ramp_kits_home=tmp_dir)
             assert_submission(
                 ramp_kit_dir=kit_dir, ramp_data_dir=kit_dir,
                 submission='starting_kit')
-        # testing assert_notebook
-        kit = RAMP_KITS_AVAILABLE[0]
-        kit_dir = fetch_ramp_kit(kit, ramp_kits_home=tmp_dir)
-        assert_notebook(ramp_kit_dir=kit_dir)
+            # testing assert_notebook on first kit
+            if test_notebook:
+                assert_notebook(ramp_kit_dir=kit_dir)
+                test_notebook = False
     finally:
         shutil.rmtree(tmp_dir)
