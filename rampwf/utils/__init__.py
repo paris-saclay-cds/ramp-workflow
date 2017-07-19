490bf1b377e44652530a50f3c08737fc56cbe1e6
Balazs Kegl <balazs.kegl@gmail.com>
2017-07-20 01:37:41 +0200

    forgotten expose

--- rampwf/utils/__init__.py
+++ rampwf/utils/__init__.py
@@ -1,12 +1,11 @@
 from .command_line import ramp_test_submission
-from .testing import assert_title
-from .testing import assert_data
-from .testing import assert_cv
-from .testing import assert_submission
+from .testing import (
+    assert_title, assert_data, assert_cv, assert_submission, assert_notebook)
 
 __all__ = ['ramp_test_submission',
            'assert_title',
            'assert_data',
            'assert_cv',
            'assert_submission',
+           'assert_notebook',
            ]
