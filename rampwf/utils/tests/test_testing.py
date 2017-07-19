92c648c50773e401c7e57d9e9ada38739583b1cd
Balazs Kegl <balazs.kegl@gmail.com>
2017-07-20 01:22:56 +0200

    test assert notebook

--- rampwf/utils/tests/test_testing.py
+++ rampwf/utils/tests/test_testing.py
@@ -1,7 +1,7 @@
 import shutil
 from tempfile import mkdtemp
 from rampwf.kits import fetch_ramp_kit
-from rampwf.utils import assert_submission
+from rampwf.utils import assert_submission, assert_notebook
 from rampwf.kits.ramp_kit import RAMP_KITS_AVAILABLE
 
 
@@ -13,5 +13,9 @@ def test_submission_all_kits():
             assert_submission(
                 ramp_kit_dir=kit_dir, ramp_data_dir=kit_dir,
                 submission='starting_kit')
+        # testing assert_notebook
+        kit = RAMP_KITS_AVAILABLE[0]
+        kit_dir = fetch_ramp_kit(kit, ramp_kits_home=tmp_dir)
+        assert_notebook(ramp_kit_dir=kit_dir)
     finally:
         shutil.rmtree(tmp_dir)
