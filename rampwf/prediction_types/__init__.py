ccd386deef675453c5073181264169428964bee6
Balazs Kegl <balazs.kegl@gmail.com>
2017-07-20 00:37:13 +0200

    Clustering stuff (for HEP tracking)

--- rampwf/prediction_types/__init__.py
+++ rampwf/prediction_types/__init__.py
@@ -1,3 +1,11 @@
+from .clustering import make_clustering
 from .combined import make_combined
 from .multiclass import make_multiclass
 from .regression import make_regression
+
+__all__ = [
+    'make_clustering',
+    'make_combined',
+    'make_multiclass',
+    'make_regression',
+]
