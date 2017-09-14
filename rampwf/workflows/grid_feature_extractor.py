b39a81be5caae9618f71cf7c040205d492ec2da0
David John Gagne <djgagne@ou.edu>
2017-09-14 17:10:24 -0600

    Updated top docstrings for workflow.

--- rampwf/workflows/grid_feature_extractor.py
+++ rampwf/workflows/grid_feature_extractor.py
@@ -1,5 +1,16 @@
+"""
+Extract features from a set of spatially gridded data.
+
+This feature extractor can be used for any problem in which one has a series of
+spatial grids arranged in time, y, x dimensions. The xarray library is used
+to read the data into a Dataset, which is represented by X_ds. 
+y_array can be a set of binary labels or a regressor value. The values in 
+y_array are assumed to be paired exactly with the values in the first 
+dimension of X_ds.
+"""
 import imp
 import numpy as np
+import pandas as pd
 
 class GridFeatureExtractor(object):
     def __init__(self, workflow_element_names=['feature_extractor']):
@@ -13,7 +24,9 @@ class GridFeatureExtractor(object):
         feature_extractor = imp.load_source(
             '', submitted_feature_extractor_file)
         fe = feature_extractor.FeatureExtractor()
-        fe.fit(X_ds.isel(enstime=train_is), y_array[train_is])
+        dim_set = list(train_X.dims.keys())
+        time_dim = dim_set[dim_set.str.contains("time")][0]
+        fe.fit(X_ds.isel(**{time_dim: train_is}), y_array[train_is])
         return fe
 
     def test_submission(self, trained_model, X_df):
