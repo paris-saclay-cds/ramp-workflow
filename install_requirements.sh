bb4cd89095bedb6a399ac5cd2f178481d1a3148f
Alexandre Boucaud <alexandre.boucaud@ias.u-psud.fr>
2017-09-15 20:16:02 +0200

    Move skimage and joblib imports to remove dependency

--- install_requirements.sh
+++ install_requirements.sh
@@ -1,6 +1,5 @@
 conda install --yes --quiet numpy scipy matplotlib scikit-learn pandas
 conda install --yes --quiet xarray gitpython
-conda install --yes --quiet scikit-image cloudpickle jupyter netcdf4
+conda install --yes --quiet cloudpickle jupyter netcdf4
 pip install tensorflow
 pip install keras
-pip install joblib
