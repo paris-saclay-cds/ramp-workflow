7645cd65c8ef8cfd8ba5cf8340c5c37d6df69fa3
Alexandre Boucaud <alexandre.boucaud@ias.u-psud.fr>
2017-09-15 20:17:41 +0200

    Remove unused dependency

--- install_requirements.sh
+++ install_requirements.sh
@@ -1,5 +1,5 @@
 conda install --yes --quiet numpy scipy matplotlib scikit-learn pandas
 conda install --yes --quiet xarray gitpython
-conda install --yes --quiet cloudpickle jupyter netcdf4
+conda install --yes --quiet jupyter netcdf4
 pip install tensorflow
 pip install keras
