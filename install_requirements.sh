a39a62492416a1c489901aabf5b3b72f7aaa0d68
Alexandre Boucaud <alexandre.boucaud@ias.u-psud.fr>
2017-09-15 20:09:27 +0200

    Put test libraries dependencies in travis

--- install_requirements.sh
+++ install_requirements.sh
@@ -1,5 +1,5 @@
-conda install --yes --quiet numpy scipy matplotlib scikit-learn pandas 
-conda install --yes --quiet xarray pytest pytest-cov gitpython 
+conda install --yes --quiet numpy scipy matplotlib scikit-learn pandas
+conda install --yes --quiet xarray gitpython
 conda install --yes --quiet scikit-image cloudpickle jupyter netcdf4
 pip install tensorflow
 pip install keras
