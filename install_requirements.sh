c52c5d5648b2116c99a4e0bdee0e3f7ea8dade0a
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2017-09-18 17:45:41 +0200

    add back joblib in install script

--- install_requirements.sh
+++ install_requirements.sh
@@ -3,3 +3,4 @@ conda install --yes --quiet xarray pytest pytest-cov gitpython
 conda install --yes --quiet scikit-image cloudpickle jupyter netcdf4
 pip install tensorflow
 pip install keras
+pip install joblib
