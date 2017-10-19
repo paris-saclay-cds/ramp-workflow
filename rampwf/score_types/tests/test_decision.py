8f9e4d75849141047c451ae5a05f954fd204f3d4
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2017-10-19 10:56:46 +0200

    Scores: fix scp for corner cases of predicted objects outside of image (#68)

--- rampwf/score_types/tests/test_decision.py
+++ rampwf/score_types/tests/test_decision.py
@@ -1,28 +1,40 @@
 import pytest
 
 from rampwf.score_types import AverageDetectionPrecision
+from rampwf.score_types.detection import scp_single
 
 
-def test_average_precision():
-    ap = AverageDetectionPrecision()
+x = [(1, 1, 1)]
 
-    # perfect match
-    y_true = [[(1, 1, 1), (3, 3, 1)]]
-    y_pred = [[(1, 1, 1, 1), (1, 3, 3, 1)]]
-    assert ap(y_true, y_pred) == 1
 
-    # imperfect match
-    y_true = [[(1, 1, 1), (3, 3, 1), (7, 7, 1), (9, 9, 1)]]
-    y_pred = [[(1, 1, 1, 1), (1, 5, 5, 1)]]
-    assert ap(y_true, y_pred) == pytest.approx(3. / 2 / 11, rel=1e-5)
-    # would be 0.125 (1 / 8) exact method
+def test_scp():
+    shape = (10, 10)
 
-    y_true = [[(1, 1, 1), (3, 3, 1), (7, 7, 1), (9, 9, 1)]]
-    y_pred = [[(1, 1, 1.2, 1.2), (1, 3, 3, 1)]]
-    assert ap(y_true, y_pred) == pytest.approx(6. / 11, rel=1e-5)
-    # would be 0.5 with exact method
+    # object outside of the image is discarded, although it actually is a
+    # bad detection
+    scp_single(x, [(20, 20, 1)], shape) == (1, 1, 0)
 
-    # no match
-    y_true = [[(1, 1, 1)]]
-    y_pred = [[(1, 3, 3, 1)]]
-    assert ap(y_true, y_pred) == 0
+
+# def test_average_precision():
+#     ap = AverageDetectionPrecision()
+#
+#     # perfect match
+#     y_true = [[(1, 1, 1), (3, 3, 1)]]
+#     y_pred = [[(1, 1, 1, 1), (1, 3, 3, 1)]]
+#     assert ap(y_true, y_pred) == 1
+#
+#     # imperfect match
+#     y_true = [[(1, 1, 1), (3, 3, 1), (7, 7, 1), (9, 9, 1)]]
+#     y_pred = [[(1, 1, 1, 1), (1, 5, 5, 1)]]
+#     assert ap(y_true, y_pred) == pytest.approx(3. / 2 / 11, rel=1e-5)
+#     # would be 0.125 (1 / 8) exact method
+#
+#     y_true = [[(1, 1, 1), (3, 3, 1), (7, 7, 1), (9, 9, 1)]]
+#     y_pred = [[(1, 1, 1.2, 1.2), (1, 3, 3, 1)]]
+#     assert ap(y_true, y_pred) == pytest.approx(6. / 11, rel=1e-5)
+#     # would be 0.5 with exact method
+#
+#     # no match
+#     y_true = [[(1, 1, 1)]]
+#     y_pred = [[(1, 3, 3, 1)]]
+#     assert ap(y_true, y_pred) == 0
