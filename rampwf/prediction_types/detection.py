053c88ad726b80f46ed8e5b3287f8f693b5fd23f
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2017-10-25 15:22:57 +0200

    basic combination of predictions

--- rampwf/prediction_types/detection.py
+++ rampwf/prediction_types/detection.py
@@ -3,9 +3,13 @@
 ``y_pred`` should be two dimensional (n_samples x n_classes).
 
 """
+import itertools
 
 import numpy as np
+
 from .base import BasePrediction
+from ..score_types.detection.iou import cc_iou
+from ..score_types.detection.util import _match_tuples
 
 
 class Predictions(BasePrediction):
@@ -28,10 +32,28 @@ class Predictions(BasePrediction):
 
     @classmethod
     def combine(cls, predictions_list, index_list=None):
-        if len(predictions_list) == 0:
-            return []
-        else:
-            return predictions_list[0]
+
+        if index_list is None:  # we combine the full list
+            index_list = range(len(predictions_list))
+        y_comb_list = [predictions_list[i].y_pred for i in index_list]
+
+        matches = []
+
+        for a, b in itertools.combinations(y_comb_list, 2):
+            idx1, idx2, ious = _match_tuples([(x, y, r) for (c, x, y, r) in a],
+                                             [(x, y, r) for (c, x, y, r) in b])
+
+            idx1 = idx1[ious > 0.5]
+            idx2 = idx2[ious > 0.5]
+
+            for i1, i2 in zip(idx1, idx2):
+                combined = (np.asarray(a[i1]) + np.array(b[i2])) / 2
+                matches.append(combined)
+
+        combined = _greedy_nms(matches)
+
+        combined_predictions = cls(y_pred=combined)
+        return combined_predictions
 
     def set_valid_in_train(self, predictions, test_is):
         """Set a cross-validation slice."""
@@ -45,3 +67,32 @@ class Predictions(BasePrediction):
 
 def make_detection():
     return Predictions
+
+
+def _greedy_nms(y_pred, iou_threshold=0.45):
+    y_pred = np.asarray(y_pred)
+
+    boxes_left = np.copy(y_pred)
+    # This is where we store the boxes that make it through the non-maximum suppression
+    maxima = []
+
+    # While there are still boxes left to compare...
+    while boxes_left.shape[0] > 0:
+        # ...get the index of the next box with the highest confidence...
+        maximum_index = np.argmax(boxes_left[:, 0])
+        # ...copy that box and...
+        maximum_box = np.copy(boxes_left[maximum_index])
+        # ...append it to `maxima` because we'll definitely keep it
+        maxima.append(maximum_box)
+        # Now remove the maximum box from `boxes_left`
+        boxes_left = np.delete(boxes_left, maximum_index, axis=0)
+        # If there are no boxes left after this step, break. Otherwise...
+        if boxes_left.shape[0] == 0:
+            break
+        # ...compare (IoU) the other left over boxes to the maximum box...
+        similarities = np.array([cc_iou(b[1:], maximum_box[1:]) for b in
+                                 boxes_left])
+        # ...so that we can remove the ones that overlap too much with the maximum box
+        boxes_left = boxes_left[similarities <= iou_threshold]
+
+    return np.array(maxima)
