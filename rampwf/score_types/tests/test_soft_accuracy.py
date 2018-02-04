d7349e60c533bb25cf211812845705a2c7c8d56c
Balazs Kegl <balazs.kegl@gmail.com>
2018-02-04 20:51:34 +0100

    Catching negative probes in soft accuracy (#101)

--- rampwf/score_types/tests/test_soft_accuracy.py
+++ rampwf/score_types/tests/test_soft_accuracy.py
@@ -23,21 +23,35 @@ y_true_proba_3 = np.array([0.5, 0.5, 0])
 
 y_proba_1 = np.array([1, 0, 0])
 y_proba_2 = np.array([0, 1, 0])
+y_proba_3 = np.array([0, 0.1, 0])
+y_proba_4 = np.array([-1, 0.1, -2])
 
 
 def test_soft_accuracy():
     score_1 = SoftAccuracy(score_matrix=score_matrix_1)
     assert score_1(np.array([y_true_proba_1]), np.array([y_proba_1])) == 1
     assert score_1(np.array([y_true_proba_1]), np.array([y_proba_2])) == 0
+    assert score_1(np.array([y_true_proba_1]), np.array([y_proba_3])) == 0
+    assert score_1(np.array([y_true_proba_1]), np.array([y_proba_4])) == 0
     assert score_1(np.array([y_true_proba_2]), np.array([y_proba_1])) == 0
     assert score_1(np.array([y_true_proba_2]), np.array([y_proba_2])) == 1
+    assert score_1(np.array([y_true_proba_2]), np.array([y_proba_3])) == 1
+    assert score_1(np.array([y_true_proba_2]), np.array([y_proba_4])) == 1
     assert score_1(np.array([y_true_proba_3]), np.array([y_proba_1])) == 0.5
     assert score_1(np.array([y_true_proba_3]), np.array([y_proba_2])) == 0.5
+    assert score_1(np.array([y_true_proba_3]), np.array([y_proba_3])) == 0.5
+    assert score_1(np.array([y_true_proba_3]), np.array([y_proba_4])) == 0.5
 
     score_2 = SoftAccuracy(score_matrix=score_matrix_2)
     assert score_2(np.array([y_true_proba_1]), np.array([y_proba_1])) == 1
     assert score_2(np.array([y_true_proba_1]), np.array([y_proba_2])) == 0.5
+    assert score_2(np.array([y_true_proba_1]), np.array([y_proba_3])) == 0.5
+    assert score_2(np.array([y_true_proba_1]), np.array([y_proba_4])) == 0.5
     assert score_2(np.array([y_true_proba_2]), np.array([y_proba_1])) == 0.3
     assert score_2(np.array([y_true_proba_2]), np.array([y_proba_2])) == 1
+    assert score_2(np.array([y_true_proba_2]), np.array([y_proba_3])) == 1
+    assert score_2(np.array([y_true_proba_2]), np.array([y_proba_4])) == 1
     assert score_2(np.array([y_true_proba_3]), np.array([y_proba_1])) == 0.65
     assert score_2(np.array([y_true_proba_3]), np.array([y_proba_2])) == 0.75
+    assert score_2(np.array([y_true_proba_3]), np.array([y_proba_3])) == 0.75
+    assert score_2(np.array([y_true_proba_3]), np.array([y_proba_4])) == 0.75
