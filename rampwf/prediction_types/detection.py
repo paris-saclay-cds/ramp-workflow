29b91d2f4c88f575c8a3b4da3c28667105ee8949
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2018-02-05 10:36:27 +0100

    make the predictions work on actual arrays of predictions (not only on single set of predictions)

--- rampwf/prediction_types/detection.py
+++ rampwf/prediction_types/detection.py
@@ -39,73 +39,22 @@ class Predictions(BasePrediction):
         if index_list is None:  # we combine the full list
             index_list = range(len(predictions_list))
         y_comb_list = [predictions_list[i].y_pred for i in index_list]
-        n_preds = len(y_comb_list)
 
-        # for all pairs of the predictions: combine its detections and keep
-        # those combinations that have a IOU > threshold
-        matches = []
-        matches_combined = []
+        n_preds = len(y_comb_list[0])
 
-        for mod1, mod2 in itertools.combinations(range(len(y_comb_list)), 2):
+        y_preds_combined = np.empty(n_preds, dtype=object)
 
-            pred1 = y_comb_list[mod1]
-            pred2 = y_comb_list[mod2]
+        for i in range(n_preds):
 
-            idx1, idx2, ious = _match_tuples(
-                [(x, y, r) for (c, x, y, r) in pred1],
-                [(x, y, r) for (c, x, y, r) in pred2])
+            preds_list = [preds[i] for preds in y_comb_list]
 
-            idx1 = idx1[ious > cls.iou_threshold]
-            idx2 = idx2[ious > cls.iou_threshold]
+            preds_combined, _ = combine_predictions(
+                preds_list, cls.iou_threshold, greedy=greedy)
 
-            # add each matching combination to list as (model_idx, pred_idx)
-            # where:
-            # model_idx is the index into y_comb_list
-            # pred_idx is the index into y_comb_list[model_idx]
-            for i1, i2 in zip(idx1, idx2):
-                comb = (np.asarray(pred1[i1]) + np.array(pred2[i2])) / 2
-                matches.append(((mod1, i1), (mod2, i2)))
-                matches_combined.append(comb)
+            y_preds_combined[i] = preds_combined
 
-        if greedy:
-            combined = _greedy_nms(matches_combined)
-            combined_predictions = cls(y_pred=combined)
-            return combined_predictions, matches
-
-        # the matches (set of (model_idx, pred_idx) tuples) are the nodes of
-        # our graph:
-        # - TODO: remove redundant edges/nodes
-        # - convert to adjacency matrix
-        # - get connected components of matrix
-        nodes = sorted(set([x for y in matches for x in y]))
-        M = create_adjacency_matrix_from_edge_list(nodes, matches)
-        match_groups = get_connected_components(nodes, M)
-
-        # each connected set of detections is combined into a single (c,x,y,r)
-        for group in match_groups:
-
-            # convert list of (model_idx, pred_idx) tuples into actual
-            # predictions
-            preds = []
-            for mod, idx in group:
-                preds.append(y_comb_list[mod][idx])
-
-            preds = np.array(preds)
-
-            # (x, y, r) are averaged weighted with the confidence
-            pred_combined = np.average(preds[:, 1:], weights=preds[:, 0],
-                                       axis=0)
-
-            # the confidence is averaged taking into account missing
-            # predictions
-            preds_combined = []
-            conf = preds[:, 0].sum() / n_preds
-            pred_combined = np.insert(pred_combined, 0, conf)
-
-            preds_combined.append(pred_combined)
-
-        combined_predictions = cls(y_pred=np.array(preds_combined))
-        return combined_predictions, matches
+        combined_predictions = cls(y_pred=y_preds_combined)
+        return combined_predictions
 
     def set_valid_in_train(self, predictions, test_is):
         """Set a cross-validation slice."""
@@ -177,3 +126,77 @@ def get_connected_components(nodes, matrix):
     a_nodes = np.empty(len(nodes), dtype=object)
     a_nodes[:] = nodes
     return [a_nodes[labels == i] for i in range(ncon)]
+
+
+def combine_predictions(preds_list, iou_threshold, greedy=False):
+    """
+    Combine multiple sets of predictions (of different models)
+    for a single patch.
+
+
+    """
+    n_preds = len(preds_list)
+
+    # for all pairs of the predictions: combine its detections and keep
+    # those combinations that have a IOU > threshold
+    matches = []
+    matches_combined = []
+
+    for mod1, mod2 in itertools.combinations(range(len(preds_list)), 2):
+
+        pred1 = preds_list[mod1]
+        pred2 = preds_list[mod2]
+
+        idx1, idx2, ious = _match_tuples(
+            [(x, y, r) for (c, x, y, r) in pred1],
+            [(x, y, r) for (c, x, y, r) in pred2])
+
+        idx1 = idx1[ious > iou_threshold]
+        idx2 = idx2[ious > iou_threshold]
+
+        # add each matching combination to list as (model_idx, pred_idx)
+        # where:
+        # model_idx is the index into preds_list
+        # pred_idx is the index into preds_list[model_idx]
+        for i1, i2 in zip(idx1, idx2):
+            comb = (np.asarray(pred1[i1]) + np.array(pred2[i2])) / 2
+            matches.append(((mod1, i1), (mod2, i2)))
+            matches_combined.append(comb)
+
+    if greedy:
+        combined = _greedy_nms(matches_combined)
+        return combined, matches
+
+    # the matches (set of (model_idx, pred_idx) tuples) are the nodes of
+    # our graph:
+    # - TODO: remove redundant edges/nodes
+    # - convert to adjacency matrix
+    # - get connected components of matrix
+    nodes = sorted(set([x for y in matches for x in y]))
+    M = create_adjacency_matrix_from_edge_list(nodes, matches)
+    match_groups = get_connected_components(nodes, M)
+
+    preds_combined = []
+
+    # each connected set of detections is combined into a single (c,x,y,r)
+    for group in match_groups:
+
+        # convert list of (model_idx, pred_idx) tuples into actual
+        # predictions
+        preds = []
+        for mod, idx in group:
+            preds.append(preds_list[mod][idx])
+
+        preds = np.array(preds)
+
+        # (x, y, r) are averaged weighted with the confidence
+        pred_combined = np.average(preds[:, 1:], weights=preds[:, 0], axis=0)
+
+        # the confidence is averaged taking into account missing
+        # predictions
+        conf = preds[:, 0].sum() / n_preds
+        pred_combined = np.insert(pred_combined, 0, conf)
+
+        preds_combined.append(pred_combined)
+
+    return preds_combined, matches
--- rampwf/prediction_types/tests/test_detection_predictions.py
+++ rampwf/prediction_types/tests/test_detection_predictions.py
@@ -6,12 +6,13 @@ from rampwf.prediction_types.detection import Predictions
 def test_combine():
 
     pred1 = Predictions(
-        y_pred=[(1, 1, 1, 1), (0.65, 3, 3, 1)])
+        y_pred=[[(1, 1, 1, 1), (0.65, 3, 3, 1)]])
     pred2 = Predictions(
-        y_pred=[(0.9, 1.1, 1.1, 1.1), (0.5, 3, 3, 1), (0.8, 5, 5, 1)])
+        y_pred=[[(0.9, 1.1, 1.1, 1.1), (0.5, 3, 3, 1), (0.8, 5, 5, 1)]])
     pred3 = Predictions(
-        y_pred=[(0.7, 3, 3, 1), (0.6, 5.1, 5.1, 1), (0.3, 10, 10, 1)])
+        y_pred=[[(0.7, 3, 3, 1), (0.6, 5.1, 5.1, 1), (0.3, 10, 10, 1)]])
 
     y_pred_combined = Predictions.combine([pred1, pred2, pred3]).y_pred
 
-    assert y_pred_combined.shape[0] == 3
+    assert y_pred_combined.shape[0] == 1
+    assert len(y_pred_combined[0]) == 3
