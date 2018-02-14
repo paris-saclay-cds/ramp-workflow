cd83036d1e9a2b61b980a83fba13047ef3150224
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2018-02-14 13:52:21 +0100

    pep8

--- rampwf/kits/tests/test_kits.py
+++ rampwf/kits/tests/test_kits.py
@@ -1,16 +1,15 @@
-from rampwf.utils.testing import assert_submission
-
 import os
 
+from rampwf.utils.testing import assert_submission
 
 
-_module_path = os.path.dirname(__file__)
+PATH = os.path.dirname(__file__)
 
 
 def test_iris():
 
     assert_submission(
-                    ramp_kit_dir=os.path.join(_module_path, "kits", "iris"),
-                    ramp_data_dir=os.path.join(_module_path, "kits", "iris"),
-                    submission='starting_kit', is_pickle=True,
-                    save_y_preds=True, retrain=True)
+        ramp_kit_dir=os.path.join(PATH, "kits", "iris"),
+        ramp_data_dir=os.path.join(PATH, "kits", "iris"),
+        submission='starting_kit', is_pickle=True,
+        save_y_preds=True, retrain=True)
