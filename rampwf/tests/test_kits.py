feac73ac79fd198e34e5164de2facb72fbd80929
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2018-02-14 14:08:59 +0100

    add boston_housing

--- rampwf/tests/test_kits.py
+++ rampwf/tests/test_kits.py
@@ -13,3 +13,12 @@ def test_iris():
         ramp_data_dir=os.path.join(PATH, "kits", "iris"),
         submission='starting_kit', is_pickle=True,
         save_y_preds=True, retrain=True)
+
+
+def test_boston_housing():
+
+    assert_submission(
+        ramp_kit_dir=os.path.join(PATH, "kits", "boston_housing"),
+        ramp_data_dir=os.path.join(PATH, "kits", "boston_housing"),
+        submission='starting_kit', is_pickle=True,
+        save_y_preds=True, retrain=True)
