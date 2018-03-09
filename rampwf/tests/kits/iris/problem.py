f6379d4de13d31a070f3962149c83a6a7a013cb9
Balazs Kegl <balazs.kegl@gmail.com>
2018-03-09 14:55:36 +0100

    commenting out brier for now

--- rampwf/tests/kits/iris/problem.py
+++ rampwf/tests/kits/iris/problem.py
@@ -17,12 +17,13 @@ score_types = [
     rw.score_types.ClassificationError(name='error'),
     rw.score_types.NegativeLogLikelihood(name='nll'),
     rw.score_types.F1Above(name='f1_70', threshold=0.7),
-    rw.score_types.BrierSkillScore(name="BSS", precision=3),
-    rw.score_types.BrierScore(name="BS", precision=3),
-    rw.score_types.BrierScoreReliability(name="BS Rel", precision=5),
-    rw.score_types.BrierScoreResolution(name="BS Res", precision=3),
+    # rw.score_types.BrierSkillScore(name="BSS", precision=3),
+    # rw.score_types.BrierScore(name="BS", precision=3),
+    # rw.score_types.BrierScoreReliability(name="BS Rel", precision=5),
+    # rw.score_types.BrierScoreResolution(name="BS Res", precision=3),
     rw.score_types.NormalizedGini(name='ngini', precision=3),
     rw.score_types.BalancedAccuracy(name='bac', precision=3),
+    rw.score_types.MacroAveragedRecall(name='mar', precision=3),
 ]
 
 
