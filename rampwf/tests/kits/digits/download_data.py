c957c700de7d9e81da9a7664193c3501dfde8631
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2018-04-03 14:00:15 +0200

    iter

--- rampwf/tests/kits/digits/download_data.py
+++ rampwf/tests/kits/digits/download_data.py
@@ -8,16 +8,20 @@ import pandas as pd
 from sklearn.datasets import load_digits
 from skimage.io import imsave
 
+KIT_DIR = os.path.dirname(__file__)
+
 
 def fetch_data():
     # create locally the images to load them afterwards
     digits = load_digits()
-    if not os.path.exists('data'):
-        os.mkdir('data')
+
+    data_dir = os.path.join(KIT_DIR, 'data')
+    if not os.path.exists(data_dir):
+        os.mkdir(data_dir)
     n_images = digits.data.shape[0]
     n_train_images = int(n_images * 0.8)
 
-    img_dir = os.path.join('data', 'imgs')
+    img_dir = os.path.join(data_dir, 'imgs')
     if not os.path.exists(img_dir):
         os.mkdir(img_dir)
 
@@ -30,8 +34,8 @@ def fetch_data():
     train_csv = pd.DataFrame({'id': np.array(filenames_image[:n_train_images]),
                               'class': digits.target[:n_train_images]})
     train_csv = train_csv.set_index('id')
-    train_csv.to_csv(os.path.join('data', 'train.csv'))
+    train_csv.to_csv(os.path.join(data_dir, 'train.csv'))
     test_csv = pd.DataFrame({'id': np.array(filenames_image[n_train_images:]),
                              'class': digits.target[n_train_images:]})
     test_csv = test_csv.set_index('id')
-    test_csv.to_csv(os.path.join('data', 'test.csv'))
+    test_csv.to_csv(os.path.join(data_dir, 'test.csv'))
