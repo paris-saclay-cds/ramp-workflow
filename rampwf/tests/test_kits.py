57f267d664092fb90c2a471eca7efafd15bc4c74
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2018-04-10 14:14:34 +0200

    [MRG] TST add (simplified and normal) image classifier test kit (#122)
    * TST add simplified image classifier test kit
    
    * iter
    
    * debug
    
    * solve issue
    
    * update test requiremetn
    
    * add tensorflow
    
    * Skip python 2.7 windows
    
    * TST skip when no tensorflow
    
    * issue python version
    
    * solve issue requirement
    
    * iter
    
    * iter
    
    * remove previous skip test
    
    * TST add image classifier
    
    * add joblib for test
    
    * use only one worker
    
    * do not use multiprocessing

--- rampwf/tests/test_kits.py
+++ rampwf/tests/test_kits.py
@@ -10,9 +10,23 @@ from rampwf.utils.testing import (
 PATH = os.path.dirname(__file__)
 
 
+def skip_no_tensorflow():
+    try:
+        import tensorflow
+    except ImportError:
+        return pytest.mark.skip(reason='tensorflow not available')
+    return pytest.mark.basic
+
+
 def _generate_grid_path_kits():
-    return [(os.path.abspath(path_kit,))
-            for path_kit in sorted(glob.glob(os.path.join(PATH, 'kits', '*')))]
+    grid = []
+    for path_kit in sorted(glob.glob(os.path.join(PATH, 'kits', '*'))):
+        if 'digits' in path_kit:
+            grid.append(pytest.param(os.path.abspath(path_kit),
+                                     marks=skip_no_tensorflow()))
+        else:
+            grid.append(os.path.abspath(path_kit))
+    return grid
 
 
 @pytest.mark.parametrize(
@@ -22,7 +36,6 @@ def _generate_grid_path_kits():
 def test_notebook_testing(path_kit):
     # check if there is a notebook to be tested
     if len(glob.glob(os.path.join(path_kit, '*.ipynb'))):
-        print(path_kit)
         assert_notebook(ramp_kit_dir=path_kit)
 
 
@@ -31,6 +44,7 @@ def test_notebook_testing(path_kit):
     _generate_grid_path_kits()
 )
 def test_submission(path_kit):
+    print(path_kit)
     submissions = sorted(glob.glob(os.path.join(path_kit, 'submissions', '*')))
     for sub in submissions:
         assert_submission(
