1e8a2673e1d5fe9e372fdaa52987259d7d304d34
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2018-04-19 17:18:50 +0200

    FIX use balanced accuracy from scikit-learn

--- rampwf/score_types/balanced_accuracy.py
+++ rampwf/score_types/balanced_accuracy.py
@@ -1,13 +1,21 @@
-"""Balanced accuracy.
-
-From https://github.com/ch-imad/AutoMl_Challenge/blob/2353ec0/Starting_kit/scoring_program/libscores.py#L187  # noqa
-
-See the thread at 
-https://github.com/rhiever/tpot/issues/108#issuecomment-317067760
-about the different definitions.
-"""
 from .classifier_base import ClassifierBaseScoreType
-from .macro_averaged_recall import MacroAveragedRecall
+from sklearn.metrics import recall_score
+from sklearn.metrics.classification import _check_targets
+
+
+def _balanced_accuracy_score(y_true, y_pred, sample_weight=None):
+    """FIXME: port implementation of balanced accuracy from scikit-learn 0.20.
+    """
+    y_type, y_true, y_pred = _check_targets(y_true, y_pred)
+
+    if y_type != 'binary':
+        raise ValueError('Balanced accuracy is only meaningful '
+                         'for binary classification problems.')
+    # simply wrap the ``recall_score`` function
+    return recall_score(y_true, y_pred,
+                        pos_label=None,
+                        average='macro',
+                        sample_weight=sample_weight)
 
 
 class BalancedAccuracy(ClassifierBaseScoreType):
@@ -20,8 +28,6 @@ class BalancedAccuracy(ClassifierBaseScoreType):
         self.precision = precision
 
     def __call__(self, y_true_label_index, y_pred_label_index):
-        mac = MacroAveragedRecall()
-        tpr = mac(y_true_label_index, y_pred_label_index)
-        base_tpr = 1. / len(self.label_names)
-        score = (tpr - base_tpr) / (1 - base_tpr)
+        score = _balanced_accuracy_score(y_true_label_index,
+                                         y_pred_label_index)
         return score
