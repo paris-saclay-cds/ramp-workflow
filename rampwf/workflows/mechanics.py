5bd4e9f9e9f307c69536938660aa5f4fa8df9cc7
yetkinyilmaz <yetkin.yilmaz@cern.ch>
2018-09-30 17:06:01 +0200

    RMSE option for cyclic values (e.g. angle)
    RMS not just by the difference but rolls for 2*pi when necessary…

--- rampwf/workflows/mechanics.py
+++
@@ -1,9 +0,0 @@
-
-from .feature_extractor_regressor import FeatureExtractorRegressor
-
-
-class Mechanics(FeatureExtractorRegressor):
-    def __init__(self, workflow_element_names=[
-            'feature_extractor', 'regressor']):
-        super(Mechanics, self).__init__(workflow_element_names)
-        self.element_names = workflow_element_names
