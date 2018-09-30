bb5ddff9f783af100bc1b6ae2ac484e95d916f14
yetkinyilmaz <yetkin.yilmaz@cern.ch>
2018-09-30 18:00:40 +0200

    modification in RMSE to for periodic target
    RMS not just by the difference but rolls for 2*pi when necessary…

--- rampwf/score_types/rmse.py
+++ rampwf/score_types/rmse.py
@@ -7,9 +7,14 @@ class RMSE(BaseScoreType):
     minimum = 0.0
     maximum = float('inf')
 
-    def __init__(self, name='rmse', precision=2):
+    def __init__(self, name='rmse', precision=2, periodicity=-1):
         self.name = name
         self.precision = precision
+        self.periodicity = -1
 
     def __call__(self, y_true, y_pred):
-        return np.sqrt(np.mean(np.square(y_true - y_pred)))
+        d = np.abs(y_true - y_pred)
+        if(self.periodicity > 0):
+            d = min(np.mod(d, self.periodicity),
+                    np.mod(-d, self.periodicity))
+        return np.sqrt(np.mean(np.square(d)))
