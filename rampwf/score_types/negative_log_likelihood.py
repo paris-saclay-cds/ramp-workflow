555aa6b501c3cc5f70ff5e772af1756bcadec795
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2018-10-02 14:58:34 +0200

    Use sklearn.metrics.log_loss score for negative log likelihood

--- rampwf/score_types/negative_log_likelihood.py
+++ rampwf/score_types/negative_log_likelihood.py
@@ -1,4 +1,6 @@
 import numpy as np
+from sklearn.metrics import log_loss
+
 from .base import BaseScoreType
 
 
@@ -7,16 +9,10 @@ class NegativeLogLikelihood(BaseScoreType):
     minimum = 0.0
     maximum = float('inf')
 
-    def __init__(self, name='negative lof likelihood', precision=2):
+    def __init__(self, name='negative log likelihood', precision=2):
         self.name = name
         self.precision = precision
 
     def __call__(self, y_true_proba, y_proba):
-        # Normalize rows
-        y_proba_normalized = y_proba / np.sum(y_proba, axis=1, keepdims=True)
-        # Kaggle's rule
-        y_proba_normalized = np.maximum(y_proba_normalized, 10 ** -15)
-        y_proba_normalized = np.minimum(y_proba_normalized, 1 - 10 ** -15)
-        scores = - np.sum(np.log(y_proba_normalized) * y_true_proba, axis=1)
-        score = np.mean(scores)
+        score = log_loss(y_true_proba, y_pred_proba)
         return score
