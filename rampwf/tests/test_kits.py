2d156c776f67d8ab60c7a896f1a216c016c73636
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2018-12-17 19:47:06 +0100

    iter

--- rampwf/tests/test_kits.py
+++ rampwf/tests/test_kits.py
@@ -62,11 +62,13 @@ def test_blending():
     assert_submission(
         ramp_kit_dir=os.path.join(PATH, "kits", "iris"),
         ramp_data_dir=os.path.join(PATH, "kits", "iris"),
+        ramp_submission_dir=os.path.join(PATH, "kits", "iris"),
         submission='starting_kit', is_pickle=True,
         save_y_preds=True, retrain=True)
     assert_submission(
         ramp_kit_dir=os.path.join(PATH, "kits", "iris"),
         ramp_data_dir=os.path.join(PATH, "kits", "iris"),
+        ramp_submission_dir=os.path.join(PATH, "kits", "iris"),
         submission='random_forest_10_10', is_pickle=True,
         save_y_preds=True, retrain=True)
     blend_submissions(
