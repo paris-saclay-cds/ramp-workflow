130b1f8cf95216255667c72f30af9a4498d0dc17
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2019-01-31 17:10:53 +0100

    fix flake8

--- rampwf/utils/cli/show.py
+++ rampwf/utils/cli/show.py
@@ -14,7 +14,7 @@ class PythonLiteralOption(click.Option):
     def type_cast_value(self, ctx, value):
         try:
             return ast.literal_eval(value)
-        except:
+        except:  # noqa: E722
             raise click.BadParameter(value)
 
 
