853e8acacd7c7ca8302496ff0adb6619999cdb59
Gabriel Hurtado <gWX760467@china.huawei.com>
2019-10-22 11:09:26 +0200

    Added uniform

--- rampwf/utils/dists_gen_regression_utils.py
+++ rampwf/utils/dists_gen_regression_utils.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def normpdf_custom(x, mean, sd):
+def norm_pdf(x, mean, sd):
     assert_normal(sd)
     var = sd ** 2
     denom = (2 * np.pi * var) ** .5
@@ -10,12 +10,30 @@ def normpdf_custom(x, mean, sd):
     probs = num / denom
     return probs
 
+def assert_normal(sd):
+    assert np.all(sd>0), "Make sure all sigmas are positive " \
+                         "(second parameter)"
+
+def uniform_pdf(x, a, b):
+    assert_uniform(a,b)
+    probs = np.zeros(x.shape)
+    probs[np.logical_and(x>=a, x<=b)] = 1./(b-a)
+    return probs
+
+def assert_uniform(a, b):
+    assert np.all(a<b), "Make sure all \"a\" (first parameter)" \
+                        " are bigger than \"b\" (second parameter)"
 
 def get_pdf_from_dist(y, type, params):
-    if type[0] == 0:
-        return normpdf_custom(y, params[:,0], params[:,1])
+
+    # All the distributions for a given dimension and a given y are the same.
+    # The value need to be repeated to still be present when doing CV
+    type_cure_dists = type[0]
+
+    if type_cure_dists == 0:
+        return norm_pdf(y, params[:,0], params[:,1])
+    if type_cure_dists == 1:
+        return uniform_pdf(y, params[:,0], params[:,1])
     return probs
 
 
-def assert_normal(sd):
-    assert np.all(sd>0), "Make sure all sigmas are positive (second parameter)"
