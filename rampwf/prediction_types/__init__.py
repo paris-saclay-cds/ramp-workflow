4a05365909678c2c1c7db78e804a2bf3e7eeab46
Balazs Kegl <b00497701@china.huawei.com>
2019-11-04 14:39:58 +0100

    rename

--- rampwf/prediction_types/__init__.py
+++ rampwf/prediction_types/__init__.py
@@ -4,14 +4,12 @@ from .detection import make_detection
 from .multiclass import make_multiclass
 from .regression import make_regression
 from .generative_regression import make_generative_regression
-from .generative_regression_dists import make_generative_regression_dists
 
 __all__ = [
     'make_clustering',
     'make_combined',
     'make_detection',
+    'make_generative_regression',
     'make_multiclass',
     'make_regression',
-    'make_generative_regression',
-    'make_generative_regression_dists'
 ]
