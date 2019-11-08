11092b13ee61e5398cc9dee1f7e10e73fba7347c
Gabriel Hurtado <gWX760467@china.huawei.com>
2019-11-08 11:00:34 +0100

    Exposed distribution dict for clearer interface, added cv based on system restarts

--- rampwf/cvs/__init__.py
+++ rampwf/cvs/__init__.py
@@ -1,7 +1,9 @@
 from .clustering import Clustering
-from .time_series import TimeSeries
+from .time_series import (TimeSeries, InsideRestart, PerRestart)
 
 __all__ = [
     'Clustering',
     'TimeSeries',
+    'InsideRestart',
+    'PerRestart',
 ]
--- rampwf/utils/__init__.py
+++ rampwf/utils/__init__.py
@@ -27,5 +27,5 @@ __all__ = ['assert_cv',
            'import_file',
            'distributions_dispatcher',
            'MAX_PARAMS',
-           'distributions_dict'
+           'distributions_dict',
            ]
