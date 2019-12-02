bc41939aed3d7cbad5bac2a715ba04296a16cd6f
Balazs Kegl <balazs.kegl@gmail.com>
2019-12-02 10:41:11 +0000

    make blending more readable

--- rampwf/utils/testing.py
+++ rampwf/utils/testing.py
@@ -252,8 +252,7 @@ def blend_submissions(submissions, ramp_kit_dir='.', ramp_data_dir='.',
             predictions_valid_list, ground_truths_valid,
             score_types[score_type_index],
             min_improvement=min_improvement)
-        print(best_index_list)
-
+        
         # we share a unit of 1. among the contributive submissions
         unit_contributivity = 1. / len(best_index_list)
         for i in best_index_list:
