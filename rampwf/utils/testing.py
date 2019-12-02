e40b5bdface761ca89dd575f4b0cb72ba1087a03
Balazs Kegl <balazs.kegl@gmail.com>
2019-12-02 11:40:26 +0000

    creating training_output before using it

--- rampwf/utils/testing.py
+++ rampwf/utils/testing.py
@@ -216,7 +216,7 @@ def blend_submissions(submissions, ramp_kit_dir='.', ramp_data_dir='.',
     print_title('Blending {}'.format(problem.problem_title))
     X_train, y_train, X_test, y_test = assert_data(
         ramp_kit_dir, ramp_data_dir, data_label)
-    cv, _, _ = assert_cv(ramp_kit_dir, ramp_data_dir, data_label)
+    cv = assert_cv(ramp_kit_dir, ramp_data_dir, data_label)
     valid_is_list = [valid_is for (train_is, valid_is) in cv]
     score_types = assert_score_types(ramp_kit_dir)
     n_folds = len(valid_is_list)
@@ -256,7 +256,7 @@ def blend_submissions(submissions, ramp_kit_dir='.', ramp_data_dir='.',
             predictions_valid_list, ground_truths_valid,
             score_types[score_type_index],
             min_improvement=min_improvement)
-        
+
         # we share a unit of 1. among the contributive submissions
         unit_contributivity = 1. / len(best_index_list)
         for i in best_index_list:
