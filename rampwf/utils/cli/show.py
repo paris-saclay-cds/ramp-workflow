ce359e5f5b6063a8defff6334165efe132ebe177
kegl <balazs.kegl@gmail.com>
2019-12-06 21:55:29 +0000

    adding bagged leaderboard

--- rampwf/utils/cli/show.py
+++ rampwf/utils/cli/show.py
@@ -67,7 +67,10 @@ def main():
               help='Sort in ascending or descending order.')
 @click.option("--precision", default=2, show_default=True,
               help='The precision for the different metrics reported.')
-def leaderboard(ramp_kit_dir, metric, step, sort_by, ascending, precision):
+@click.option("--bagged/--mean", default=True, show_default=True,
+              help='Bagged or mean scores.')
+def leaderboard(ramp_kit_dir, metric, step, sort_by, ascending, precision,
+                bagged):
     """Display the leaderboard for all the local submissions."""
     path_submissions = os.path.join(ramp_kit_dir, 'submissions')
     all_submissions = {
@@ -75,27 +78,50 @@ def leaderboard(ramp_kit_dir, metric, step, sort_by, ascending, precision):
         for sub in os.listdir(path_submissions)
         if os.path.isdir(os.path.join(path_submissions, sub))
     }
-    data = {}
-    for sub_name, sub_path in all_submissions.items():
-        scores = _load_score_submission(sub_path, metric, step)
-        if scores is None:
-            continue
-        data[sub_name] = scores
-    df = pd.concat(data, names=['submission'])
-    df = df.unstack(level=['step'])
-    df = pd.concat([df.groupby('submission').mean(),
-                    df.groupby('submission').std()],
-                   keys=['mean', 'std'], axis=1, names=['stat'])
-    df = df.round(precision).reorder_levels([1, 2, 0], axis=1)
-    step = ['train', 'valid', 'test'] if not step else step
-    df = (df.sort_index(axis=1, level=0)
-            .reindex(labels=step, level='step', axis=1))
+    if bagged:  # bagged scores
+        subs = []
+        valid_scores = []
+        test_scores = []
+        for sub, path in all_submissions.items():
+            bagged_scores_path = os.path.join(
+                path, 'training_output', 'bagged_scores.csv')
+            if not os.path.isfile(bagged_scores_path):
+                continue
+            bagged_scores_df = pd.read_csv(bagged_scores_path)
+            n_folds = len(bagged_scores_df) // 2
+            subs.append(sub)
+            valid_scores.append(bagged_scores_df.iloc[n_folds - 1, 2])
+            test_scores.append(bagged_scores_df.iloc[2 * n_folds - 1, 2])
+            metric = bagged_scores_df.columns[2]
+        df = pd.DataFrame()
+        df['submission'] = subs
+        df['valid {}'.format(metric)] = valid_scores
+        df['test {}'.format(metric)] = test_scores
+        headers = df.columns.get_values()
+    else:  # mean scores with std
+        data = {}
+        for sub_name, sub_path in all_submissions.items():
+            scores = _load_score_submission(sub_path, metric, step)
+            if scores is None:
+                continue
+            data[sub_name] = scores
+        df = pd.concat(data, names=['submission'])
+        df = df.unstack(level=['step'])
+        df = pd.concat([df.groupby('submission').mean(),
+                        df.groupby('submission').std()],
+                       keys=['mean', 'std'], axis=1, names=['stat'])
+        df = df.reorder_levels([1, 2, 0], axis=1)
+        step = ['train', 'valid', 'test'] if not step else step
+        df = (df.sort_index(axis=1, level=0)
+                .reindex(labels=step, level='step', axis=1))
+        headers = (["\n".join(df.columns.names)] +
+                   ["\n".join(col_names) for col_names in df.columns.get_values()])
 
+    df = df.round(precision)
+    print(df.columns)
     if sort_by:
-        df = df.sort_values(tuple(sort_by), ascending=ascending, axis=0)
+        df = df.sort_values(sort_by, ascending=ascending, axis=0)
 
-    headers = (["\n".join(df.columns.names)] +
-               ["\n".join(col_names) for col_names in df.columns.get_values()])
     click.echo(tabulate(df, headers=headers, tablefmt='grid'))
 
 
