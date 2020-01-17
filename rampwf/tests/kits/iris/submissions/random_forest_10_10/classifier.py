4798e1039811b8233ef3c5bbfaa891ff08a1197f
Lucy Liu <jliu176@gmail.com>
2020-01-17 20:14:36 +0100

    format

--- rampwf/tests/kits/iris/submissions/random_forest_10_10/classifier.py
+++ rampwf/tests/kits/iris/submissions/random_forest_10_10/classifier.py
@@ -2,6 +2,6 @@ from sklearn.ensemble import RandomForestClassifier
 
 
 def get_pipeline():
-    clf = RandomForestClassifier(
-        n_estimators=10, max_leaf_nodes=10, random_state=61)
+    clf = RandomForestClassifier(n_estimators=10, max_leaf_nodes=10,
+                                 random_state=61)
     return clf
--- rampwf/tests/kits/iris/submissions/starting_kit/classifier.py
+++ rampwf/tests/kits/iris/submissions/starting_kit/classifier.py
@@ -1,8 +1,7 @@
-from sklearn.base import BaseEstimator
 from sklearn.ensemble import RandomForestClassifier
 
 
 def get_pipeline():
-    clf = RandomForestClassifier(
-        n_estimators=1, max_leaf_nodes=2, random_state=61)
+    clf = RandomForestClassifier(n_estimators=1, max_leaf_nodes=2,
+                                 random_state=61)
     return clf
