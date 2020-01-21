6483e8b7c22191107b8697eeaddc01eb06874fcf
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2020-01-21 20:24:35 +0100

    MNT add cloudpickle as a dependence

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ CLASSIFIERS = ['Intended Audience :: Science/Research',
                'Programming Language :: Python :: 3.6',
                'Programming Language :: Python :: 3.7']
 INSTALL_REQUIRES = ['numpy', 'scipy', 'pandas', 'scikit-learn', 'joblib',
-                    'click']
+                    'cloudpickle', 'click']
 EXTRAS_REQUIRE = {
     'tests': ['pytest', 'pytest-cov'],
     'docs': ['sphinx', 'sphinx_rtd_theme', 'numpydoc', 'sphinx-click']
