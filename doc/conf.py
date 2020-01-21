f87133ff28dc8a1c82a33156c9b41d51e115a69c
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2020-01-21 17:40:26 +0100

    MNT remove unecessary import in doc

--- doc/conf.py
+++ doc/conf.py
@@ -21,7 +21,6 @@ import sphinx_rtd_theme
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('sphinxext'))
 from github_link import make_linkcode_resolve
-import generate_database_schema
 
 # -- General configuration ------------------------------------------------
 
