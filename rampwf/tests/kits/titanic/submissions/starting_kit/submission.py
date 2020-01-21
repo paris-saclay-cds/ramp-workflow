2d3176fcc8b0eaf7ebf4592685c65f972aa82a8f
Lucy Liu <jliu176@gmail.com>
2020-01-21 19:10:21 +0100

    amend nb for new wkfl, update submission

--- rampwf/tests/kits/titanic/submissions/starting_kit/submission.py
+++ rampwf/tests/kits/titanic/submissions/starting_kit/submission.py
@@ -1,18 +1,23 @@
-from sklearn.pipeline import Pipeline
 from sklearn.compose import make_column_transformer
+from sklearn.preprocessing import OneHotEncoder
 from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
 from sklearn.linear_model import LogisticRegression
 
 
 def get_estimator():
 
-    numeric_cols = ['Pclass', 'Age', 'SibSp', 'Parch',
-                    'Fare']
+    categorical_cols = ['Sex', 'Pclass', 'Embarked']
+    numerical_cols = ['Age', 'SibSp', 'Parch', 'Fare']
+
     preprocessor = make_column_transformer(
-        (SimpleImputer(strategy='median'), numeric_cols),
+        (OneHotEncoder(handle_unknown='ignore'), categorical_cols),
+        (SimpleImputer(strategy='constant', fill_value=-1), numerical_cols),
     )
-    pipeline = Pipeline(steps=[
+
+    pipeline = Pipeline([
         ('transformer', preprocessor),
         ('classifier', LogisticRegression()),
     ])
+
     return pipeline
