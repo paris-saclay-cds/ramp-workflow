fba4ecf254341d8183f2a9c0ee9f61e513a95392
Albert Thomas <albertthomas88@gmail.com>
2020-11-25 19:01:03 +0100

    missing MAX_MDN_PARAMS

--- rampwf/prediction_types/tests/test_generative_regression.py
+++ rampwf/prediction_types/tests/test_generative_regression.py
@@ -4,7 +4,7 @@ import numpy as np
 from numpy.testing import assert_array_equal
 
 from rampwf.prediction_types import make_generative_regression
-from rampwf.utils import MAX_MDN_PARAMS
+from rampwf.utils import MAX_MIXTURE_PARAMS
 
 
 def test_init_empty():
@@ -16,10 +16,10 @@ def test_init_empty():
     predictions = Predictions(n_samples=2)
     assert_array_equal(
         predictions.y_pred, np.array(
-            [[np.nan] * (1 + (2 + MAX_MDN_PARAMS))] * 2))
+            [[np.nan] * (1 + (2 + MAX_MIXTURE_PARAMS))] * 2))
     max_dists = 4
     Predictions = make_generative_regression(max_dists, label_names=[1, 2])
     predictions = Predictions(n_samples=2)
     assert_array_equal(
         predictions.y_pred, np.array(
-            [[np.nan] * 2 * (1 + (2 + MAX_MDN_PARAMS) * max_dists)] * 2))
+            [[np.nan] * 2 * (1 + (2 + MAX_MIXTURE_PARAMS) * max_dists)] * 2))
