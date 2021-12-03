8e2aefa2f075fbf4d4c4ae599fb6b0d76b5ed571
Martin Tabikh <martin.tabikh@gmail.com>
2021-12-03 18:01:54 +0100

    add one hyperopt folder

---
+++ rampwf/hyperopt/tests/interfaces/header_in_files/classifier_kit/submissions/one_hyper_kit_hyperopt/classifier.py
@@ -0,0 +1,26 @@
+from sklearn.linear_model import LogisticRegression
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.base import BaseEstimator
+from rampwf.hyperopt import Hyperparameter
+
+# test with only one hyperparameter
+# RAMP START HYPERPARAMETERS
+logreg_C = Hyperparameter(
+	dtype='float', default=0.9, values=[0.01, 0.1, 0.9, 1.0, ])
+# RAMP END HYPERPARAMETERS
+
+
+class Classifier(BaseEstimator):
+    def __init__(self):
+        self.clf = Pipeline([
+            ('imputer',
+             SimpleImputer(strategy='median')),
+            ('classifier', LogisticRegression(C=float(logreg_C)))
+        ])
+
+    def fit(self, X, y):
+        self.clf.fit(X, y)
+
+    def predict_proba(self, X):
+        return self.clf.predict_proba(X)
