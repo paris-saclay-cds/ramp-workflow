7bc5501a51a431ec569a4f7c73ab91b511a6d58a
Dimitri Papadopoulos <3234522+DimitriPapadopoulos@users.noreply.github.com>
2021-12-16 16:46:47 +0100

    Update colored from 1.3.5 to 1.4.3
    The repository has been moved from GitHub to GitLab:
    https://gitlab.com/dslackw/colored/

--- rampwf/externals/colored/colors.py
+++ rampwf/externals/colored/colors.py
@@ -1,27 +1,5 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-"""
-colors.py is a part of colored.
-
-Copyright 2014-2017 Dimitris Zlatanidis <d.zlatanidis@gmail.com>
-All rights reserved.
-
-Colored is very simple Python library for color and formatting in terminal.
-
-https://github.com/dslackw/colored
-
-colored is free software: you can redistribute it and/or
-modify it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
-You should have received a copy of the GNU General Public License
-along with this program. If not, see <http://www.gnu.org/licenses/>.
-"""
 
 
 names = [
