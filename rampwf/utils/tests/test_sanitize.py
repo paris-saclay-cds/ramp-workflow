3b71b62c2639effc4de40fa1cc3f46446ee5cf0c
Thomas Moreau <thomas.moreau.2010@gmail.com>
2022-03-09 15:34:18 +0100

    ENH allow open in user code for non-os moudles (#303)
    For some challenges, users need to load images with Image.open
    from the PIL library. This is not possible as _sanitize_input
    is too restrictive. This commits intend to change this.

--- rampwf/utils/tests/test_sanitize.py
+++ rampwf/utils/tests/test_sanitize.py
@@ -10,6 +10,12 @@ def test_sanitize_input():
     with pytest.raises(RuntimeError, match=msg):
         _sanitize_input("with open('test.txt', 'wr') as fh")
 
+    with pytest.raises(RuntimeError, match=msg):
+        _sanitize_input("f = os.open('test.txt', 'wr')")
+
+    # Make sure this does not catch open functions from other libraries:
+    _sanitize_input("im = Image.open('im001.jpg')")
+
     msg = "forbidden key word scandir detected"
     with pytest.raises(RuntimeError, match=msg):
         _sanitize_input("for _ in os.scandir()")
