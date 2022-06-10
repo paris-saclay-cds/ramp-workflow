bc22dca3e241fb1684445980432c39d1713c8309
Balazs Kegl <balazs.kegl@gmail.com>
2022-06-10 21:09:21 +0800

    exposing write_hyperparameters

--- rampwf/hyperopt/__init__.py
+++ rampwf/hyperopt/__init__.py
@@ -1,10 +1,11 @@
 from .hyperopt import (
         Hyperparameter, init_hyperopt, parse_all_hyperparameters,
-        run_hyperopt)
+        run_hyperopt, write_hyperparameters)
 
 __all__ = [
     'Hyperparameter',
     'init_hyperopt',
     'parse_all_hyperparameters',
     'run_hyperopt',
+    'write_hyperparameters',
 ]
