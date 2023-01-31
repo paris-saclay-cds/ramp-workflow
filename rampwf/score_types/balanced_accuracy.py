4f7b0d9b49015c4b7c7fd9bfa9905199abcfd43b
Francois Caud <frcaud@gmail.com>
2023-01-31 12:39:08 +0100

    bacc with 'adjusted' parameter

--- rampwf/score_types/balanced_accuracy.py
+++ rampwf/score_types/balanced_accuracy.py
@@ -1,13 +1,15 @@
 """Balanced accuracy.
 
-From https://github.com/scikit-learn/scikit-learn/blob/98cf537f5/sklearn/metrics/_classification.py#L2111
+From https://github.com/scikit-learn/scikit-learn/blob/98cf537f5/sklearn/\
+    metrics/_classification.py#L2111
 
     The balanced accuracy in binary and multiclass classification problems to
     deal with imbalanced datasets. It is defined as the average of recall
-    obtained on each class.
+    obtained on each class. With the use of the parameter 'adjusted', 
+    balanced accuracy can be adjusted between 1/(1-nclasses) and 1.
 """
 from .classifier_base import ClassifierBaseScoreType
-from .macro_averaged_recall import MacroAveragedRecall
+from sklearn.metrics import balanced_accuracy_score
 
 
 class BalancedAccuracy(ClassifierBaseScoreType):
@@ -15,12 +17,22 @@ class BalancedAccuracy(ClassifierBaseScoreType):
     minimum = 0.0
     maximum = 1.0
 
-    def __init__(self, name='balanced_accuracy', precision=2):
+    def __init__(self, name='balanced_accuracy', precision=2, adjusted=True):
         self.name = name
         self.precision = precision
+        self.adjusted = adjusted
 
     def __call__(self, y_true_label_index, y_pred_label_index):
-        mac = MacroAveragedRecall()
-        bac = mac(y_true_label_index, y_pred_label_index)
+        """
+        When adjusted = True, it will use an adjusted balanced_accuracy_score
+        from sklearn which is calculated by subtracting the base true positive
+        rate (i.e. the chance recall) from the macro averaged recall, and dividing
+        the result by (1 - base true positive rate). Score will then be between
+        1 / (1 - nclasses) and 1.
+        When adjusted = False, it will use the non-adjusted balanced_accuracy_score
+        from sklearn.
+        """
+        score = balanced_accuracy_score(
+            y_true_label_index, y_pred_label_index, adjusted=self.adjusted)
 
-        return bac
+        return score
