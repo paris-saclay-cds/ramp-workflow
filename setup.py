935d3ea8ed30a97b6010cafc13c60b352c41d2c9
Albert Thomas <albertthomas88@gmail.com>
2024-03-07 15:01:37 +0100

    rm ramp-hyperopt command in setup.py

--- setup.py
+++ setup.py
@@ -62,5 +62,4 @@ setup(
             'ramp-blend = rampwf.utils.cli.blend:start',
             'ramp-test = rampwf.utils.cli.testing:start',
             'ramp-show = rampwf.utils.cli.show:start',
-            'ramp-hyperopt = rampwf.hyperopt.cli.hyperopt:start',
         ]})
