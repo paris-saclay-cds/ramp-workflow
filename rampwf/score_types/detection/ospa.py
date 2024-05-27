1d8a06d34631047464cbdee75c222cbda5c03c0f
Balazs Kegl <balazs.kegl@gmail.com>
2024-05-27 20:53:44 +0800

    [bug] sklearn new version

--- rampwf/score_types/detection/ospa.py
+++ rampwf/score_types/detection/ospa.py
@@ -1,6 +1,8 @@
 import numpy as np
-from sklearn.utils import indices_to_mask
-
+try:
+    from sklearn.utils._mask import indices_to_mask
+except ImportError:
+    from sklearn.utils import indices_to_mask
 from .base import DetectionBaseScoreType
 from .util import _select_minipatch_tuples, _match_tuples
 
