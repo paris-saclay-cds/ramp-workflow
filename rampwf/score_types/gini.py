45f9d1504f100b36af825b50a168161bf3b021a4
Balazs Kegl <balazs.kegl@gmail.com>
2024-11-19 01:33:46 +0800

    gini score

---
+++ rampwf/score_types/gini.py
@@ -0,0 +1,31 @@
+import numpy as np
+from .classifier_base import ClassifierBaseScoreType
+
+
+def gini(y_true, y_pred, cmpcol=0, sortcol=1):
+    all = np.asarray(
+        np.c_[y_true, y_pred, np.arange(len(y_true))], dtype=float)
+    all = all[np.lexsort((all[:, 2], -all[:, 1]))]
+    total_losses = all[:, 0].sum()
+    giniSum = all[:, 0].cumsum().sum() / total_losses
+    giniSum -= (len(y_true) + 1) / 2.
+    return giniSum / len(y_true)
+
+
+class Gini(ClassifierBaseScoreType):
+    is_lower_the_better = False
+    minimum = -1.0
+    maximum = 1.0
+
+    def __init__(self, name='gini', precision=2):
+        self.name = name
+        self.precision = precision
+
+    def score_function(self, ground_truths, predictions):
+        y_proba = predictions.y_pred[:, 1]
+        y_true_proba = ground_truths.y_pred_label_index
+        self.check_y_pred_dimensions(y_true_proba, y_proba)
+        return self.__call__(y_true_proba, y_proba)
+
+    def __call__(self, y_true_proba, y_proba):
+        return gini(y_true_proba, y_proba)
